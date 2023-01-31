84a699f1b7f63f06ed3386dad9ba09dfdc8ce314
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-01-30 23:32:36 -0300

    :tada: Commit inicial

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    highlight: {
+      "100": "#FFBA08",
+      "50": "#C09D43",
+    },
+    dark: {
+      "900": "#000000",
+      "500": "#47585B",
+      "300": "#999999",
+      "50": "#7F7F7F",
+    },
+    light: {
+      "900": "#FFFFFF",
+      "500": "#F5F8FA",
+      "100": "#DADADA",
+    }
+  }
+})
