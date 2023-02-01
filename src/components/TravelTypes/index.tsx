0a9c9f231e536f4aef2df3935233c3dd034410bb
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-02-01 00:04:52 -0300

    :construction: feat: Pagina home

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -1,19 +1,28 @@
-import Types from './types';
-import nightLife from "../../../public/images_project/types/Nightlife.png"
-import beach from "../../../public/images_project/types/Beach.png"
-import modern from "../../../public/images_project/types/Modern.png"
-import classic from "../../../public/images_project/types/Classic.png"
-import more from "../../../public/images_project/types/More.png"
-import { Box } from '@chakra-ui/react';
+import Types from "./types";
+import nightLife from "../../../public/images_project/types/Nightlife.png";
+import beach from "../../../public/images_project/types/Beach.png";
+import modern from "../../../public/images_project/types/Modern.png";
+import classic from "../../../public/images_project/types/Classic.png";
+import more from "../../../public/images_project/types/More.png";
+import { Box } from "@chakra-ui/react";
 
 export function TravelTypes() {
   return (
-    <Box display="flex" w="100%" p="20" alignItems="center" justifyContent="space-between">
+    <Box
+      display="flex"
+      w="100%"
+      pt="20"
+      pr="20"
+      pl="20"
+      pb="10"
+      alignItems="center"
+      justifyContent="space-between"
+    >
       <Types src={nightLife} alt="Night life" />
       <Types src={beach} alt="Beach" />
       <Types src={modern} alt="Modern" />
       <Types src={classic} alt="Classic" />
       <Types src={more} alt="More" />
     </Box>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from "@/components/Header";
 import { TravelTypes } from '@/components/TravelTypes';
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Divider, Flex, Text, theme } from "@chakra-ui/react";
 import Image from "next/image";
 import banner from "../../public/images_project/Banner.png";
 
@@ -12,6 +12,13 @@ export default function Home() {
         <Image src={banner} alt="banner" width="1480" />
       </Box>
       <TravelTypes />
+      <Center height='50px'>
+        <Divider orientation='horizontal' h={1} w={20} bg="dark.500"/>
+      </Center>
+      <Flex flexDirection="column" align="center" p={15}>
+        <Text fontSize={30}>Vamos nessa?</Text>
+        <Text fontSize={30}>Então escolha o seu continente</Text>
+      </Flex>
     </Box>
   );
 }
